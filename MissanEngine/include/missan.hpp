89a35bd9fabb7c4cb0c782c15fe711a1c0e0d63a
henrik syvertsson <henrik.syvertsson@gmail.com>
2023-07-26 15:11:56 +0300

    make MissanEngine into static lib

---
+++ MissanEngine/include/missan.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/projection.hpp>
+
+#include <imgui/imgui.h>
+
+#include "ecs.hpp"
+#include "engine.hpp"
+#include "input.hpp"
+#include "inspectable.hpp"
+#include "transform.hpp"
+#include "collider.hpp"
+#include "physics.hpp"
+#include "rigidbody.hpp"
+#include "window.hpp"
+#include "camera.hpp"
+#include "color.hpp"
+#include "light.hpp"
+#include "material.hpp"
+#include "mesh.hpp"
+#include "renderer.hpp"
+#include "shader.hpp"
+#include "texture.hpp"
